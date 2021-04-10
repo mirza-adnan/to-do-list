614f1b5a2b6a62c528bf78e423516a9ae6574f03
mizzadnan <mirza.adnan2205@gmail.com>
2021-04-10 18:36:51 +0600

    redoing the js

--- src/dom.js
+++ src/dom.js
@@ -1,12 +1,7 @@
-import Project from "./projects.js";
-import app from "./app";
-import Default from "./defaultProjects.js";
 const dom = (function() {
 
   function init() {
     toggleNav();
-    newProjectButtons();
-    createDefaultProjects()
   }
 
   function toggleNav() {
@@ -15,51 +10,6 @@ const dom = (function() {
         document.querySelector("nav").classList.toggle("nav-hide");
         document.querySelector("main").classList.toggle("main-toggle-margin");
       })
-  }
-
-  function getProjectForm() {
-    return document.querySelector(".project-form");
-  }
-
-  function getNewProjectButton() {
-    return document.querySelector(".new-project-btn");
-  }
-
-  function newProjectButtons() {
-    const newProjectButton = getNewProjectButton();
-    const cancelProjectButton = document.querySelector(".cancel-project-btn");
-    const addProjectButton = document.querySelector(".add-project-btn");
-    newProjectButton.addEventListener("click", toggleProjectForm);
-    cancelProjectButton.addEventListener("click", cancelProject);
-    addProjectButton.addEventListener("click", addProject);
-  }
-
-  function cancelProject() {
-    clearProjectInput();
-    toggleProjectForm();
-  }
-
-  function addProject() {
-    const projectNameInput = document.querySelector(".project-name-input");
-    const projectName = projectNameInput.value;
-    const project = new Project(projectName);
-    app.addProject(project);
-    renderProjects();
-    toggleProjectForm();
-    clearProjectInput();
-  }
-
-  function renderProjects() {
-    const newProjectsList = document.querySelector(".new-projects");
-    app.projects.forEach(project => {
-      newProjectsList.appendChild(project.element);
-    })
-  }
-
-  function createDefaultProjects() {
-    const inbox = new Default("Inbox", "inbox");
-    const today = new Default("Today", "today");
-    const thisWeek = new Default("This Week", "this-week");
   }
 
   function toggleProjectForm() {
--- src/projects.js
+++ src/projects.js
@@ -1,35 +1,20 @@
-class Project {
+export default class Project {
   constructor(name) {
     this.name = name;
     this.tasks = [];
-    this.addTask = function(task) {
-      this.tasks.push(task);
-    }
-    this.removeTask = function(task) {
-      this.tasks.forEach(task => {
-        this.tasks.splice(this.tasks.indexOf(task), 1);
-      })
-    }
-    this.makeElement = function() {
-      const li = document.createElement("li");
-      li.classList.add("project", "new-project");
-      const div = document.createElement("div");
-      div.classList.add("new-project-name");
-      const folderIcon = document.createElement("img");
-      folderIcon.src = "./img/folder.svg";
-      const projectName = document.createElement("p");
-      projectName.textContent = this.name;
-      div.appendChild(folderIcon);
-      div.appendChild(projectName);
-      const deleteButton = document.createElement("img");
-      deleteButton.src = "./img/delete.svg";
-      li.appendChild(div);
-      li.appendChild(deleteButton);
-      
-      return li;
-    }
-    this.element = this.makeElement();
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  addTask(newTask) {
+    this.tasks.forEach(task => {
+      if task.name === new 
+    })
   }
 }
-
-export default Project;
